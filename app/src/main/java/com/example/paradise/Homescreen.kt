43f5ac410097a3ca1a809dcde948b6d8ac89a024
Mohamed Kaif <mohamedkaif525@gmail.com>
2021-06-19 15:09:54 +0530

    Commit

--- app/src/main/java/com/example/paradise/Homescreen.kt
+++ app/src/main/java/com/example/paradise/Homescreen.kt
@@ -1,6 +1,7 @@
 package com.example.paradise
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -19,6 +20,8 @@ class Homescreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        Log.i("Mohamed", "Hello")
+
         val view =  inflater.inflate(R.layout.fragment_homescreen, container, false)
 
         val adapter = AllPostAdapter()
@@ -36,7 +39,6 @@ class Homescreen : Fragment() {
 
         return view
     }
-
 
 }
 
